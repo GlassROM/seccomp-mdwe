f6270a24a1cb52fcd3d31f4acea230278773d5f9
randomhydrosol <randomhydrosol@glassrom.org>
2024-06-10 00:25:46 +0000

    initial commit
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Requires clang and lld to be installed
+FLAGS='-march=x86-64 -mtune=generic -pipe -fno-plt -fexceptions -fwrapv -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -Wall -Wextra -Werror -fstack-clash-protection -fPIC -fomit-frame-pointer -ftrivial-auto-var-init=zero -fcf-protection -D_FORTIFY_SOURCE=3 -O3 -funroll-loops -fdata-sections -ffunction-sections -flto -fvisibility=hidden -fsanitize=cfi -fstack-protector-all -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs -flto=auto -Wl,-O3 -Wl,-z,noexecstack -Wl,-pie -Wl,--strip-all -Wl,--sort-common -Wl,--no-undefined -Wl,-z,now -Wl,-z,relro -Wl,-O3,--as-needed,-z,defs,-z,relro,-z,now,-z,nodlopen,-z,text -flto -Wl,--gc-sections -lhardened_malloc -fuse-ld=lld'
+
+clang seccomp-mdwe.c -DGL_PERMISSIVE -lseccomp $FLAGS -o seccomp-error
+clang seccomp-mdwe.c -lseccomp $FLAGS -o seccomp-strict
---
+++ seccomp-mdwe.c
@@ -0,0 +1,77 @@
+#include <errno.h>
+#include <seccomp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/prctl.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#ifdef GL_PERMISSIVE
+#define GL_SC_ACTION SCMP_ACT_ERRNO(EPERM)
+#else
+#define GL_SC_ACTION SCMP_ACT_KILL
+#endif
+
+void setup_seccomp() {
+  scmp_filter_ctx ctx;
+
+  ctx = seccomp_init(SCMP_ACT_ALLOW);
+  if (!ctx) {
+    perror("seccomp not supported");
+    exit(EXIT_FAILURE);
+  }
+
+  if (seccomp_rule_add(ctx, GL_SC_ACTION, SCMP_SYS(mprotect), 1,
+                       SCMP_A2(SCMP_CMP_MASKED_EQ, PROT_EXEC, PROT_EXEC)) < 0) {
+    perror("mprotect could not be protected, failing safely!");
+    seccomp_release(ctx);
+    exit(EXIT_FAILURE);
+  }
+
+  if (seccomp_rule_add(ctx, GL_SC_ACTION, SCMP_SYS(mmap), 1,
+                       SCMP_A2(SCMP_CMP_MASKED_EQ, PROT_EXEC | PROT_WRITE,
+                               PROT_EXEC | PROT_WRITE)) < 0) {
+    perror("mmap could not be protected, failing safely!");
+    seccomp_release(ctx);
+    exit(EXIT_FAILURE);
+  }
+
+  if (seccomp_rule_add(ctx, GL_SC_ACTION, SCMP_SYS(pkey_mprotect), 1,
+                       SCMP_A2(SCMP_CMP_MASKED_EQ, PROT_EXEC, PROT_EXEC)) < 0) {
+    perror("pkey_mprotect could not be protected, failing safely!");
+    seccomp_release(ctx);
+    exit(EXIT_FAILURE);
+  }
+
+  if (seccomp_rule_add(ctx, GL_SC_ACTION, SCMP_SYS(shmat), 1,
+                       SCMP_A2(SCMP_CMP_MASKED_EQ, SHM_EXEC, SHM_EXEC)) < 0) {
+    perror("shmat could not be protected, failing safely!");
+    seccomp_release(ctx);
+    exit(EXIT_FAILURE);
+  }
+
+  if (seccomp_load(ctx) < 0) {
+    perror("failed to load seccomp filter");
+    seccomp_release(ctx);
+    exit(EXIT_FAILURE);
+  }
+
+  seccomp_release(ctx);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  printf("Setting up seccomp filters...\n");
+  setup_seccomp();
+  printf("Seccomp filters set up successfully.\n");
+
+  execvp(argv[1], &argv[1]);
+
+  // Should not get here
+  perror("something went horribly wrong");
+  return EXIT_FAILURE;
+}
